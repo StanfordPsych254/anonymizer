046257cb63c932f9529a2172b0db66887fbacbdd
Michael C. Frank <mcfrank@stanford.edu>
2016-03-19 15:49:16 -0700

    relative path

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Anonymize workerids when using [cosub](https://github.com/longouyang/cosub).
 
 Assumes that workerids are in `./production-results`.
 
-Run by typing `python ./anonymize-workerids.py` from the root directory of your project repository. 
+Run by typing `python ./anonymize-workerids.py foo` where foo is the path of your project repository, e.g. `~/username/project_repo`. 
 
 
--- anonymize_workerids.py
+++ anonymize_workerids.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # Run this from repo root to anonymize workerids in production-results directory
 
-import os, json
+import os, sys, json
 
-if not os.path.isdir("./production-results"):
-    error("Directory 'production-results' not found! Run this from the root directory of your project or check that you have data stored by cosub correctly.")
+target_dir = sys.argv[1] if len(sys.argv) >= 2 else "."
 
-hits = os.listdir("./production-results/")
+if not os.path.isdir(target_dir + "/production-results"):
+    raise ValueError("\n\nDirectory 'production-results' not found at " + target_dir + "! \n\nRun this from the root directory of your project or check that you have data stored by cosub correctly.")
 
-hitlist = open("./production-results/anonymized-results-key.csv", "w")
+hits = os.listdir(target_dir + "/production-results/")
+
+hitlist = open(target_dir + "/production-results/anonymized-results-key.csv", "w")
 hitlist.write("WorkerId,AnonId\n")
 
-if not os.path.isdir("./anonymized-results/"):
-    os.mkdir("./anonymized-results")
+if not os.path.isdir(target_dir + "/anonymized-results/"):
+    os.mkdir(target_dir + "/anonymized-results")
 
 for hitnum, hit in enumerate(hits):
     if hit.endswith(".json"):
-        hitdata = json.load(open("./production-results/" + hit))
+        hitdata = json.load(open(target_dir + "/production-results/" + hit))
         workerid = hitdata["WorkerId"]
         hitdata["WorkerId"] = "anon" + str(hitnum)
-        anonfile = open("./anonymized-results/anon-" + str(hitnum) + ".json","w")
+        anonfile = open(target_dir + "/anonymized-results/anon-" + str(hitnum) + ".json","w")
         json.dump(hitdata, anonfile)
+        anonfile.close()
         hitlist.write(workerid + ",anon-" + str(hitnum) + "\n")
+
+hitlist.close()
